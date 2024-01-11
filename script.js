239ef8b0f462660962f0cc596d5ca55085be748c
Saniya-mulla03 <140803877+Saniya-mulla03@users.noreply.github.com>
2024-01-11 17:05:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputElement = document.getElementById("todo-input");
+const todoListElement = document.getElementById("todo-list");
+
+function handleOnClick(){
+    if(inputElement.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        const todoCard = document.createElement("div");
+        todoCard.classList.add("todo-card");
+        const heading3 = document.createElement("h3");
+        heading3.innerHTML = inputElement.value;
+        heading3.classList.add("checked");
+        const span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        todoCard.appendChild(heading3);
+        todoListElement.appendChild(todoCard);
+        todoCard.appendChild(span);
+
+        inputElement.value = "";
+        saveData();
+    }
+}
+
+todoListElement.addEventListener("click",function(e){
+    if(e.target.tagName === "h3"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", todoListElement.innerHTML);
+}
+
+function showTask(){
+    todoListElement.innerHTML = localStorage.getItem("data");
+}
+
+showTask(); 
